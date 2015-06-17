ffd9ed8a2b5047fea48056a4b9e2d422bfcd7005
Soroush Haeri <soroosh.haeri@icloud.com>
2015-06-17 10:47:21 -0700

    Added the experiment-parameters.cc to the CMake Compile list, fixed the bug in the brite-handler not it selects the minimum of (numbNeighbors and numnodes) to make sure no error will occur this is because BRITE does not check this and if you ask it to generate a network with 2 nodes and each node with 3 neighbors it will crash. Also, fixed the bug in the GRC node embedding.

--- src/experiments/test/experiments-test.cc
+++ src/experiments/test/experiments-test.cc
@@ -41,30 +41,62 @@ using namespace vne::utilities;
 
 BOOST_AUTO_TEST_SUITE (AlgorithmExperiments)
 
-BOOST_AUTO_TEST_CASE(MCVNE_MCF_VNR_TESTS)
+BOOST_AUTO_TEST_CASE(ARRIVAL_RATE_TESTS)
 {
-    //std::string vnr_dirs[] =
-    //	{"r-2000-50-20-10-10-10-50", "r-2000-50-50-20-10-10-50", "r-2000-50-50-20-10-15-50","r-2000-50-50-20-10-5-25"};
-    //std::string vnr_dirs[] =
-    //    	{"r-2000-50-50-20-10-5-25"};
     std::string vnr_dirs[] =
-        	{"r-10-50-50-20-10-10-50"};
-    int num_sims[] = {1};
-	for (int i=0; i<1; i++)
+        	{"reqs-12-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-14-1000-nodesMin-3-nodesMax-10-grid-25"
+      	, "reqs-16-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-20-1000-nodesMin-3-nodesMax-10-grid-25",
+			"reqs-25-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-33-1000-nodesMin-3-nodesMax-10-grid-25",
+        	"reqs-50-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-100-1000-nodesMin-3-nodesMax-10-grid-25"};
+
+
+
+
+    //MCVNE MCF - BFS simulator
+	for (int j = 0; j < 8; j++)
 	{
-		bool ret = ConfigManager::Instance()->setConfig("MCTS.MCTSParameters.NumSimulations", num_sims[i]);
-		assert (ret);
-		for (int j = 0; j < 1; j++)
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		std::string str1 = "BFS-SP";
+		ret = ConfigManager::Instance()->setConfig("MCVNE.NodeEmbeddingAlgo.LinkEmbedder", str1);
+		vne::experiments::MCVNENodeMCFLinkExp<> exp = vne::experiments::MCVNENodeMCFLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"mcvne_bfs_mcf_" <<  vnr_dirs[j] <<".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::MCVNENodeMCFLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+	}
+
+
+		// MCVNE MCF MCF
+	for (int j = 0; j < 8; j++)
 		{
-			ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+			bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
 			assert(ret);
-			Logger::Instance()->logInfo("Running MCVNE_MCF_NUMSIMS Experiment");
+			std::string str1 = "MCF";
+			ret = ConfigManager::Instance()->setConfig("MCVNE.NodeEmbeddingAlgo.LinkEmbedder", str1);
+			assert(ret);
 			vne::experiments::MCVNENodeMCFLinkExp<> exp = vne::experiments::MCVNENodeMCFLinkExp<> ();
 
 			std::string dbPath;
 			std::stringstream dbName;
 			dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
-			dbName << dbPath <<"mcvne_mcf_VNRs_" <<  vnr_dirs[j] << "_numSims_"<< num_sims[i] <<".db";
+			dbName << dbPath <<"mcvne_mcf_mcf_" <<  vnr_dirs[j] <<".db";
 			std::string str = dbName.str();
 			std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
 
@@ -72,7 +104,7 @@ BOOST_AUTO_TEST_CASE(MCVNE_MCF_VNR_TESTS)
 
 
 			db->registerBeanClass<vne::experiments::MCVNENodeMCFLinkExp<>>();
-            db->dropModel();
+			db->dropModel();
 			db->createModel();
 			db->copyBean(exp);
 
@@ -81,33 +113,227 @@ BOOST_AUTO_TEST_CASE(MCVNE_MCF_VNR_TESTS)
 			IdGenerator::Destroy();
 			RNG::Destroy();
 		}
+
+	//MCVNE BFS - BFS simulator
+	for (int j = 0; j < 8; j++)
+	{
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		std::string str1 = "BFS-SP";
+		ret = ConfigManager::Instance()->setConfig("MCVNE.NodeEmbeddingAlgo.LinkEmbedder", str1);
+		vne::experiments::MCVNENodeBFSLinkExp<> exp = vne::experiments::MCVNENodeBFSLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"mcvne_bfs_bfs_" <<  vnr_dirs[j] << ".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::MCVNENodeBFSLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+	}
+
+	//GRC MCF
+	for (int j = 0; j < 8; j++)
+	{
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		vne::experiments::GRCNodeMCFLinkExp<> exp = vne::experiments::GRCNodeMCFLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"grc_mcf_" <<  vnr_dirs[j] << ".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::GRCNodeMCFLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+	}
+
+	//GRC BFS
+	for (int j = 0; j < 8; j++)
+	{
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		vne::experiments::GRCNodeBFSLinkExp<> exp = vne::experiments::GRCNodeBFSLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"grc_bfs_" <<  vnr_dirs[j] << ".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::GRCNodeBFSLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+	}
+
+	//Vineyard Deterministic
+	for (int j = 0; j < 8; j++)
+	{
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		vne::experiments::VineNodeMCFLinkExp<> exp = vne::experiments::VineNodeMCFLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"vineyard_deterministic_" <<  vnr_dirs[j] << ".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::VineNodeMCFLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+	}
+
+	//Vineyard Randomized
+	for (int j = 0; j < 8; j++)
+	{
+		bool ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[j]);
+		assert(ret);
+		std::string str1 = "randomized";
+		ret = ConfigManager::Instance()->setConfig("vineyard.Configs.nodeMappingType", str1);
+		assert(ret);
+		vne::experiments::VineNodeMCFLinkExp<> exp = vne::experiments::VineNodeMCFLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"vineyard_randomized_" <<  vnr_dirs[j] << ".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::VineNodeMCFLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
 	}
 }
 
-BOOST_AUTO_TEST_CASE(MCVNE_MCF_NUMSIMS_TEST)
+BOOST_AUTO_TEST_CASE(MCVNE_NUMSIMS_TEST)
 {
-    for (int i = 30; i>=5; i-=5)
+	std::string vnr_dirs[] =
+	        	{"reqs-12-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-14-1000-nodesMin-3-nodesMax-10-grid-25"
+	      	, "reqs-16-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-20-1000-nodesMin-3-nodesMax-10-grid-25",
+				"reqs-25-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-33-1000-nodesMin-3-nodesMax-10-grid-25",
+	        	"reqs-50-1000-nodesMin-3-nodesMax-10-grid-25", "reqs-100-1000-nodesMin-3-nodesMax-10-grid-25"};
+
+	int numSims[] = {5,10,20,40,50,75,100,250};
+
+	// MCVNE BFS BFS
+    for (int i = 0; i<8; i++)
     {
-    	bool ret = ConfigManager::Instance()->setConfig("MCTS.MCTSParameters.NumSimulations", i);
+    	bool ret = ConfigManager::Instance()->setConfig("MCTS.MCTSParameters.NumSimulations", numSims[i]);
     	assert (ret);
-    	Logger::Instance()->logInfo("Running MCVNE_MCF_NUMSIMS Experiment");
+    	ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[6]);
+    	assert (ret);
+		std::string str1 = "BFS-SP";
+		ret = ConfigManager::Instance()->setConfig("MCVNE.NodeEmbeddingAlgo.LinkEmbedder", str1);
+		assert (ret);
+    	vne::experiments::MCVNENodeBFSLinkExp<> exp = vne::experiments::MCVNENodeBFSLinkExp<> ();
+
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"mcvne_bfs_bfs_" <<  vnr_dirs[6] << "_" << numSims[i] <<".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+
+		exp.run();
+
+
+		db->registerBeanClass<vne::experiments::MCVNENodeBFSLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
+    }
+
+	//MCVNE MCF MCF
+    for (int i = 0; i<8; i++)
+    {
+    	bool ret = ConfigManager::Instance()->setConfig("MCTS.MCTSParameters.NumSimulations", numSims[i]);
+    	assert (ret);
+    	ret = ConfigManager::Instance()->setConfig("vineyard.VirtualNetRequest.dir", vnr_dirs[6]);
+    	assert (ret);
+		std::string str1 = "MCF";
+		ret = ConfigManager::Instance()->setConfig("MCVNE.NodeEmbeddingAlgo.LinkEmbedder", str1);
+		assert (ret);
     	vne::experiments::MCVNENodeMCFLinkExp<> exp = vne::experiments::MCVNENodeMCFLinkExp<> ();
 
-    	std::string dbPath;
-    	std::stringstream dbName;
-    	dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
-    	dbName << dbPath <<"mcvne_mcf_numsims_" << i << ".db";
-    	std::string str = dbName.str();
-    	std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
+		std::string dbPath;
+		std::stringstream dbName;
+		dbPath = ConfigManager::Instance()->getConfig<std::string>("core.dbPath");
+		dbName << dbPath <<"mcvne_mcf_mcf_" <<  vnr_dirs[6] << "_" << numSims[i] <<".db";
+		std::string str = dbName.str();
+		std::shared_ptr<hiberlite::Database> db = DBManager::Instance()->createDB(str);
 
-    	exp.run();
+		exp.run();
 
-    	db->registerBeanClass<vne::experiments::MCVNENodeMCFLinkExp<>>();
-    	db->dropModel();
-    	db->createModel();
-    	db->copyBean(exp);
 
-    	ConfigManager::Destroy();
+		db->registerBeanClass<vne::experiments::MCVNENodeMCFLinkExp<>>();
+		db->dropModel();
+		db->createModel();
+		db->copyBean(exp);
+
+		//destroy all singleton classes to start fresh for next simulations
+		ConfigManager::Destroy();
+		IdGenerator::Destroy();
+		RNG::Destroy();
     }
 }
             
@@ -189,7 +415,7 @@ BOOST_AUTO_TEST_CASE(Vine_MCF)
                 
     exp.run();
                 
-    db->registerBeanClass<vne::experiments::MCVNENodeBFSLinkExp<>>();
+    db->registerBeanClass<vne::experiments::VineNodeMCFLinkExp<>>();
     db->dropModel();
     db->createModel();
     db->copyBean(exp);
